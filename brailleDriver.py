653d35e5337e510003cf8925e4a1d558baa57baf
b4ux1t3 <flippedbauxite@gmail.com>
2014-06-23 14:53:06 -0400

    Create brailleDriver.py

---
+++ brailleDriver.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+import sys
+
+# Retrieves a letter from user, returns the letter's braille array
+def setLetter():
+    pass
+
+# Parses braille arrays into GPIO output
+def parse():
+    pass
